e7cdd661e69136ab88f17acca4c2af5091d2710f
eliezerraj <eliezerraj@gmail.com>
2025-08-19 23:46:32 -0300

    feat:add ctx timeout

--- cmd/main.go
+++ cmd/main.go
@@ -55,8 +55,7 @@ func init(){
 func main (){
 	childLogger.Info().Str("func","main").Interface("appServer",appServer).Send()
 
-	ctx, cancel := context.WithTimeout(	context.Background(), 
-										time.Duration( appServer.Server.ReadTimeout ) * time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// Open Database
